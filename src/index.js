c40c15e07036102c58bbca0ca00b204a5f3a0cc4
Liza <kamauliza2@gmail.com>
2024-04-26 01:01:43 +0300

    Adding basic search engine

---
+++ src/index.js
@@ -0,0 +1,8 @@
+function search(event) {
+  event.preventDefault();
+  let searchFormInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchFormInput.value;
+}
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", search);
